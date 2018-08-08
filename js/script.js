1934116ce5e293f6070999cf3a901cbf3de3f2d4
Satya Bose <satyabose.uiux@gmail.com>
2018-08-07 23:11:56 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,212 @@
+$(document).ready(function(){
+	var ctx = $("#sketch").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 80,
+		color: "cornflowerblue",
+		},
+		{
+		value: 20,
+		color: "lightgrey",
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#balsamiq").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 60,
+		color: "cornflowerblue",
+		},
+		{
+		value: 40,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#photoshop").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 100,
+		color: "cornflowerblue",
+		},
+		{
+		value: 0,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#illus").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 90,
+		color: "cornflowerblue",
+		},
+		{
+		value: 10,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#axure").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 80,
+		color: "cornflowerblue",
+		},
+		{
+		value: 20,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#xd").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 50,
+		color: "cornflowerblue",
+		},
+		{
+		value: 50,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#html").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 90,
+		color: "cornflowerblue",
+		},
+		{
+		value: 10,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#css").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 80,
+		color: "cornflowerblue",
+		},
+		{
+		value: 20,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#sass").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 60,
+		color: "cornflowerblue",
+		},
+		{
+		value: 40,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#bootstrap").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 80,
+		color: "cornflowerblue",
+		},
+		{
+		value: 20,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#jq").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 70,
+		color: "cornflowerblue",
+		},
+		{
+		value: 30,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#js").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 60,
+		color: "cornflowerblue",
+		},
+		{
+		value: 40,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#ng").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 30,
+		color: "cornflowerblue",
+		},
+		{
+		value: 70,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+	
+	var ctx = $("#wp").get(0).getContext("2d");
+
+	var data = [
+		{
+		value: 50,
+		color: "cornflowerblue",
+		},
+		{
+		value: 50,
+		color: "lightgrey",			
+		}
+	];
+
+	var chart = new Chart(ctx).Doughnut(data);
+});
+
